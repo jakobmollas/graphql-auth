8c890e13ef3c30ac7f04d0d6189885770f6e0786
Jakob Möllås <jakob.mollas@gmail.com>
2021-10-10 17:59:48 +0200

    introduce nullability

---
+++ src/server/Security/UserRoles.cs
@@ -0,0 +1,18 @@
+﻿namespace Server.Security
+{
+    //public enum UserRole
+    //{
+    //    /// <summary>
+    //    /// Undefined - no access (fallback if no role is specified to avoid falling back to a defined role)
+    //    /// </summary>
+    //    Undefined = 0,
+    //    Read,
+    //    Write
+    //}
+
+    internal static class UserRoles
+    {
+        public const string Read = "Read";
+        public const string Write = "Write";
+    }
+}
